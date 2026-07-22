2bff86c3644c6ed881fc3bdd4157c0e15577e978
Kyle Cuss <cussprogramming@gmail.com>
2026-07-22 20:51:06 -0230

    Added movement defaults for guards

--- Source/SentinelComplex/Private/Characters/SCGuardCharacter.cpp
+++ Source/SentinelComplex/Private/Characters/SCGuardCharacter.cpp
@@ -2,12 +2,27 @@
 
 #include "SentinelComplex/Public/Characters/SCGuardCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 
 ASCGuardCharacter::ASCGuardCharacter()
 {
 	PrimaryActorTick.bStartWithTickEnabled = false;
 	PrimaryActorTick.bCanEverTick = false;
 	SetReplicates(true);
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	UCharacterMovementComponent* Movement = GetCharacterMovement();
+	Movement->bOrientRotationToMovement = true;
+	Movement->bUseControllerDesiredRotation = false;
+	Movement->RotationRate = FRotator(0.0f, 360.0f, 0.0f);
+	Movement->MaxWalkSpeed = 220.0f;
+	Movement->MaxAcceleration = 700.0f;
+	Movement->BrakingDecelerationWalking = 700.0f;
+	Movement->bRequestedMoveUseAcceleration = true;
 }
 
 ASCPatrolRoute* ASCGuardCharacter::GetPatrolRoute() const
